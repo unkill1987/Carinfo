5a100a1675b0378619ffa1f09baa4f25aa7d6cb2
unkill <unkill1987@naver.com>
2018-12-16 21:48:18 +0900

    차량정보관리
    서버용

--- carinfo/settings.py
+++ carinfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ea6^bx9p^3fbvul6^6xo17wc%v8ed9wzy+&od&xq%+zur63-ey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','pythonanywhere.com']
 
 
 # Application definition
