ef3256e509de3b44f9d992d79e4e351bad505db4
unkill <unkill1987@naver.com>
2018-12-20 18:24:40 +0900

    차량정보관리
    서버용
    
    paginations

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Notice
+from app.models import Notice, Manufacture, Government, Repairshop, Insurance
 
+admin.site.register(Manufacture)
+admin.site.register(Government)
+admin.site.register(Repairshop)
+admin.site.register(Insurance)
 admin.site.register(Notice)
